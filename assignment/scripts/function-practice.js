285817fa5c5bc399547ef91027187acc8fb140a0
Connor Ferris <ferr0373@gmail.com>
2023-01-07 15:01:33 -0600

    added #9

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -126,11 +126,19 @@ console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll(array) {
   let sum = 0
   // TODO: loop to add items
+  for(let number of array) {
+    sum += number;
+  } 
   return sum;
 }
+
+let arr = [1,2,4,5];
+
+console.log('sum all arr', sumAll(arr));
+
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
